4eeb1e21ba9753c89ef9342c95a715450cc173d3
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-08-22 20:50:15 +0300

    finally committed some codes

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,30 @@
+import { Settings } from "lucide-react";
+
+import { Heading } from "@/components/heading";
+
+import { Button } from "@/components/ui/button";
+
+
+const SettingsPage = async () => {
+
+  return ( 
+    <div>
+      <Heading
+        title="Settings"
+        description="Manage account settings."
+        icon={Settings}
+        iconColor="text-gray-700"
+        bgColor="bg-gray-700/10"
+      />
+      <div className="px-4 lg:px-8 space-y-4">
+        <div className="text-muted-foreground text-sm">
+        </div>
+        <Button>
+            Back to Dashboard
+        </Button>
+      </div>
+    </div>
+   );
+}
+ 
+export default SettingsPage;
